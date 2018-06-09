97fbf60ada8bc0532e6f396c3e49829e70948eae
Mock <arch.gp@yandex.com>
2018-06-09 18:05:05 +0530

    test34

--- test_flask.py
+++ test_flask.py
@@ -9,7 +9,7 @@ r = requests.get('http://0.0.0.0:5000/')
 
 class SimpleFlaskTest(unittest.TestCase):
 	response = r.text
-	flag = False
+	flag = True
 	def test_flask(self):
 		print(self.__class__.response)
 		self.assertTrue(self.__class__.flag,msg="the api json response it not fine")
